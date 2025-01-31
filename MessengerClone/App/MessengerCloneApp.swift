a1c761d27b5b6b33345c2fb1f2e2e000cd69d438
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2025-01-31 13:15:17 +0000

    Initial Commit

---
+++ MessengerClone/App/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Mehmet Alp Sönmez on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessengerClone/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessengerClone
+//
+//  Created by Mehmet Alp Sönmez on 31/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
