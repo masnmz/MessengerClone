e2696a231b3986947895d3dac09b9d89c9ffed7f
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2025-01-31 20:24:45 +0000

    Profile Photo Selection, ProfileViewModel

--- MessengerClone/Core/Profile/View/ProfileView.swift
+++ MessengerClone/Core/Profile/View/ProfileView.swift
@@ -5,32 +5,46 @@
 //  Created by Mehmet Alp Sönmez on 31/01/2025.
 //
 
+import PhotosUI
 import SwiftUI
 
+
 struct ProfileView: View {
+    @State var viewModel = ProfileViewModel()
     var body: some View {
         //            header
         VStack {
-            Image(systemName: "person.circle.fill")
-                .resizable()
-                .frame(width: 80, height: 80)
-                .foregroundStyle(Color(.systemGray4))
+            PhotosPicker(selection: $viewModel.selectedItem){
+                if let profileImage = viewModel.profileImage {
+                    profileImage
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+                        
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                    .foregroundStyle(Color(.systemGray4)) }
+            }
+            
             Text("Bruce Wayne")
                 .font(.title)
                 .fontWeight(.semibold)
-//            list
+            //            list
             
             List {
                 Section {
                     ForEach(SettingOptionsViewModel.allCases) { option in
-                    HStack {
-                        Image(systemName: option.imageName)
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .foregroundStyle(option.imageBackgroundColor)
-                        Text(option.title)
-                            .font(.headline)
-                        
+                        HStack {
+                            Image(systemName: option.imageName)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(option.imageBackgroundColor)
+                            Text(option.title)
+                                .font(.headline)
+                            
                         }
                     }
                 }
---
+++ MessengerClone/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewModel.swift
+//  MessengerClone
+//
+//  Created by Mehmet Alp Sönmez on 31/01/2025.
+//
+
+import Observation
+import PhotosUI
+import SwiftUI
+
+
+
+@Observable
+class ProfileViewModel: ObservableObject {
+    var selectedItem: PhotosPickerItem? {
+        didSet {
+            Task { try await loadImage()
+            }
+        }
+    }
+    
+    var profileImage: Image?
+    
+    func loadImage() async throws {
+        guard let item = selectedItem else { return }
+        guard let imageData = try await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: imageData) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+        
+    }
+}
