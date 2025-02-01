d40c2bed5e329cd5faa127de5ed2cc72cce49e16
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2025-02-01 18:21:00 +0000

    Chat UI

---
+++ MessengerClone/Core/Chat/ChatView.swift
@@ -0,0 +1,61 @@
+//
+//  ChatView.swift
+//  MessengerClone
+//
+//  Created by Mehmet Alp Sönmez on 01/02/2025.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State private var messageText = ""
+    var body: some View {
+        VStack {
+            ScrollView {
+                //            header
+                VStack {
+                    CircularProfileImageView(user: User.MockUser, size: .xLarge)
+                    
+                    VStack(spacing: 4) {
+                        Text("Bruce Wayne")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        
+                        Text("Messenger")
+                            .font(.footnote)
+                            .foregroundStyle(.gray)
+                    }
+                    
+                }
+                //            messages
+
+                
+                
+            }
+            //            message inout view
+            Spacer()
+            
+            ZStack(alignment: .trailing) {
+                TextField("Message...", text: $messageText, axis: .vertical)
+                    .padding(12)
+                    .padding(.trailing, 48)
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                Button {
+                    print("Send message")
+                } label: {
+                    Text("Send")
+                        .fontWeight(.semibold)
+                }
+                .padding(.horizontal)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ChatView()
+}
